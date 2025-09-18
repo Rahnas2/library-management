66049132129b93f7c532848f6d421650b41d853a
Rahnas p <rahnasx2@gmail.com>
2025-09-18 13:00:35 +0530

    fix: token secret missmatch issue

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -9,7 +9,7 @@ if (!authHeader || !authHeader.startsWith('Bearer ')) return res.status(401).jso
 
 const token = authHeader.split(' ')[1];
 try {
-const decoded = jwt.verify(token, process.env.JWT_SECRET);
+const decoded = jwt.verify(token, process.env.ACCESS_JWT_SECRET);
 const user = await User.findById(decoded.id).select('-password');
 if (!user) return res.status(401).json({ message: 'Not authorized' });
 req.user = user;
